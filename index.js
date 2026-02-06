4dcfd862f1e1042804863f62c6dbc9e46228e5b0
Tony Virtanen <tounivee@gmail.com>
2026-02-06 15:21:57 +0200

    Part 3, partial mangoose

--- index.js
+++ index.js
@@ -1,117 +1,130 @@
-const express = require('express')
-var morgan = require('morgan')
+require("dotenv").config();
+const express = require("express");
+var morgan = require("morgan");
 
-const app = express()
+const Person = require("./models/person");
+
+const app = express();
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 const nameExists = (name) => {
-  return persons.some(person => String(person.name) === String(name))
-}
+  return persons.some((person) => String(person.name) === String(name));
+};
 
 const generateId = (max = 1000000000) => {
   return Math.floor(Math.random() * max);
-}
+};
 
-app.use(express.json())
+app.use(express.json());
 
 // Create token + Stringify
-morgan.token('body', (req) => {
-  return JSON.stringify(req.body)
-})
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
 
 // Log request
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 // First check if the dist directory contains a file corresponding to the request's address
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
+    return response.status(400).json({
+      error: "name missing",
+    });
   } else if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
+    return response.status(400).json({
+      error: "number missing",
+    });
   } else if (nameExists(body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
+    return response.status(400).json({
+      error: "name must be unique",
+    });
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
-    number: body.number,
-  }
+    number: body.number || false,
+  });
 
-  persons = persons.concat(person)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
 
-  response.json(person)
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/info", async (req, res) => {
+  const total = await Person.countDocuments();
 
-app.get('/info', (req, res) => {
   res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
+    <p>Phonebook has info for ${total} people</p>
     <p>${new Date()}</p>
-  `)
-})
+  `);
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  // response.json(persons);
+  Person.find({}).then((result) => {
+    response.json(result);
+  });
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).end();
+    });
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
-  console.log(persons)
+  // console.log(persons);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+// if (process.argv.length < 3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
+
+if (process.argv.length < 5) {
+  console.log('not enough arguments to create a new item')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://tounivee_db_user:${password}@cluster0.jar9pon.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number,
+})
+
+person.save().then(result => {
+  console.log(`added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
+})
+
+// Person.find({}).then((result) => {
+//   result.forEach((person) => {
+//     console.log(person)
+//   })
+//   mongoose.connection.close()
+// })
